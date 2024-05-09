779723e12b54ddf642a0a636138fb8ace604c99a
GhaidaAmour <Ghaida.amour@gmail.com>
2024-05-09 05:08:36 +0300

    Get All Rating By Clinic ID

--- backend/controllers/ratings.js
+++ backend/controllers/ratings.js
@@ -4,18 +4,15 @@ const { pool } = require("../models/db");
 const createRatingByUserIdForClinic = (req, res) => {
   const { comment, rating } = req.body;
   const userId = req.token.userId;
-  const clinicId = req.params.clinicId; // Correctly access clinicId from req.params
+  const clinicId = req.params.clinicId; 
   const ratingDate = new Date(); 
 
-  // Check if the clinic exists before inserting the rating
   pool.query('SELECT * FROM clinics WHERE id = $1', [clinicId])
     .then((clinicResult) => {
-      // Check if the clinic exists
       if (clinicResult.rows.length === 0) {
         throw new Error('Clinic not found');
       }
 
-      // Insert the rating into the ratings table
       const query = `INSERT INTO ratings (comment, rating, rating_date, clinic_id, user_id) VALUES ($1, $2, $3, $4, $5) RETURNING *`;
       const values = [comment, rating, ratingDate, clinicId, userId];
       
@@ -44,8 +41,42 @@ const createRatingByUserIdForClinic = (req, res) => {
     });
 };
 
+// بدنا نجيب معدل التقييم لكل عيادة عن طريق (الاي دي) الخاص بالعيادة
+const getAllRatingByClinicId = (req, res) => {
+  const clinicId = req.params.clinicId;
+  const query = 'SELECT AVG(rating) AS average_rating FROM ratings WHERE clinic_id = $1';
+  const values = [clinicId];
+
+  pool.query(query, values)
+    .then((result) => {
+      if (result.rows[0].average_rating === null) {
+        return res.status(404).json({
+          success: false,
+          message: `No ratings found for clinic with ID ${clinicId}`,
+          result: null
+        });
+      }
+      const averageRating = result.rows[0].average_rating;
+      res.status(200).json({
+        success: true,
+        message: `Average Rating Of Clinic_ID =  ${clinicId}`,
+        result: {
+          clinic_id: clinicId,
+          average_rating: averageRating
+        }
+      });
+    })
+    .catch((error) => {
+      console.error('Error executing SQL query:', error);
+      res.status(500).json({
+        success: false,
+        message: 'Internal Server Error',
+        result: null
+      });
+    });
+};
 
 module.exports = {
   createRatingByUserIdForClinic,
+  getAllRatingByClinicId,
 };
-// :clincId/
--- backend/routes/ratings.js
+++ backend/routes/ratings.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const rateRouter = express.Router();
-const { createRatingByUserIdForClinic } = require("../controllers/ratings");
+const { createRatingByUserIdForClinic,getAllRatingByClinicId } = require("../controllers/ratings");
 // Import users controllers
 const authentication=require('../middleware/authentication')
 
 rateRouter.post("/:clinicId/",authentication,createRatingByUserIdForClinic);
+rateRouter.get("/:clinicId",getAllRatingByClinicId);
 
 module.exports = rateRouter;
 
