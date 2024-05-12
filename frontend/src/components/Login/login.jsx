e7fbec58b17306606855abe306bc10dd23790c0e
GhaidaAmour <Ghaida.amour@gmail.com>
2024-05-13 00:00:33 +0300

    login components

---
+++ frontend/src/components/Login/login.jsx
@@ -0,0 +1,94 @@
+import React, {useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+// import "./style.css";
+
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { setLogin, setUserId } from "../../service/redux/reducers/auth/authSlice";
+
+//====================================================================
+
+function login() {
+  const dispatch = useDispatch();
+  const {isLoggedIn}= useSelector((state) => {
+    return {
+      // token : state.auth.token,
+      isLoggedIn :state.auth.isLoggedIn,
+      // userId : state.auth.userId
+    };
+  });
+  const history = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [password, setPassword] = useState("");
+  const [status, setSztatus] = useState(false);
+
+  //===============================================================
+  const Login = async (e) => {
+    console.log(isLoggedIn);
+    e.preventDefault();
+    try {
+      const result = await axios.post("http://localhost:5000/users/login", {
+        email,
+        password,
+      });
+      if (result.data) {
+        console.log(result.data);
+        setMessage("");
+        dispatch(setLogin(result.data.token))
+        dispatch(setUserId(result.data.userId))
+      } else throw Error;
+    } catch (error) {
+      console.log(error);
+      if (error.response && error.response.data) {
+        return setMessage(error.response.data.message);
+      }
+      setMessage("Error happened while Login, please try again");
+    }
+  };
+
+  //===============================================================
+  useEffect(() => {
+    if (isLoggedIn) {
+      history("/");
+    }
+  });
+
+  //===============================================================
+  return (
+    <>
+      <div className="Form">
+        <p className="Title">Login:</p>
+        <form onSubmit={Login}>
+          <br />
+
+          <input
+            type="email"
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <br />
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <br />
+          <button
+            onClick={(e) => {
+              login(e);
+            }}
+          >
+            Login
+          </button>
+        </form>
+
+        {status
+          ? message && <div className="SuccessMessage">{message}</div>
+          : message && <div className="ErrorMessage">{message}</div>}
+      </div>
+    </>
+  );
+}
+export default login
--- frontend/src/service/redux/store.jsx
+++ frontend/src/service/redux/store.jsx
@@ -1,13 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import authReducer from "../redux/reducers/auth/authSlice.jsx";
-import clincReducer from "../redux/reducers/clinics/clinicSlice.jsx";
-import clinicSpecializatioReducer from "../redux/reducers/specialization/clinicSpecialization.jsx";
+// import clincReducer from "../redux/reducers/clinics/clinicSlice.jsx";
+// import clinicSpecializatioReducer from "../redux/reducers/specialization/clinicSpecialization.jsx";
 
 export default configureStore({
   reducer: {
     auth: authReducer,
-    clinic: clincReducer,
-    clinicSpecialization: clinicSpecializatioReducer,
+    // clinic: clincReducer,
+    // clinicSpecialization: clinicSpecializatioReducer,
   },
 });
