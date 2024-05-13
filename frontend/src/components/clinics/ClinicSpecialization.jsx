526f8f09951ac195f44393b5ad4045cf61158861
AhmadNGhanimah <ahmad.n.ghanimah@gmail.com>
2024-05-13 20:25:05 +0300

    clinic spectilatin

--- frontend/src/components/clinics/ClinicSpecialization.jsx
+++ frontend/src/components/clinics/ClinicSpecialization.jsx
@@ -1,13 +1,26 @@
-import React from "react";
-import "./clinic.css";
+import React, { useState } from "react";
+import { Routes, Route, useNavigate, Link, useParams } from "react-router-dom";
+import { IoLocation } from "react-icons/io5";
+import { FaUserMd } from "react-icons/fa";
+import { Rating } from "react-simple-star-rating";
 import backgroundImage from "./top-clinic.png";
+import testImageDoctor from "./testImageDoctor.jpg";
+import "./clinic.css";
+
 export default function ClinicSpecialization() {
+  const [rating, setRating] = useState(1);
+
+  const handleRating = (rating) => {
+    setRating(rating);
+    console.log(rating);
+  };
+
   return (
     <>
       <section className="clinic-specialization">
         <div
           className="all-title-box"
-          style={{ backgroundImage: `url(${backgroundImage}) ` }}
+          style={{ backgroundImage: `url(${backgroundImage})` }}
         >
           <div className="container">
             <div className="row">
@@ -18,71 +31,80 @@ export default function ClinicSpecialization() {
           </div>
         </div>
       </section>
-      <section className="doctor">
-        <div className="doctorCard">
-          <div className="preview-card">
-            <div className="preview-card__wrp">
-              <div
-                className="preview-card__item"
-                style={{ display: "flex", alignItems: "center" }}
-              >
-                <div className="preview-card__img">
-                  <img
-                    src="https://res.cloudinary.com/muhammederdem/image/upload/v1535759872/kuldar-kalvik-799168-unsplash.jpg"
-                    alt=""
-                    style={{
-                      width: "100%",
-                      height: "100%",
-                      objectFit: "cover",
-                      display: "block",
-                      borderRadius: "20px",
-                    }}
-                  />
-                  <div></div>
+      <Link to="/page-doctor">
+        <section className="doctor container">
+          <div className="card bg-light-subtle mt-4">
+            <img src={testImageDoctor} className="card-img-top" alt="Doctor" />
+            <div className="card-body">
+              <div className="text-section">
+                <h5 className="card-title fw-bold text-capitalize">
+                  <span>Doctor</span> <a href="#">Jamal Khalil</a>
+                </h5>
+                <p className="card-text">Dentist</p>
+                <div className="star">
+                  <Rating onClick={handleRating} />
                 </div>
-                <div className="preview-card__content">
-                  <span
-                    className="preview-card__code"
-                    style={{
-                      color: "#7b7992",
-                      fontWeight: "500",
-                      marginBottom: "15px",
-                      display: "block",
-                    }}
-                  >
-                    26 December 2019
-                  </span>
-                  <div
-                    className="preview-card__title"
-                    style={{
-                      fontSize: "24px",
-                      fontWeight: "700",
-                      color: "#0d0925",
-                      marginBottom: "20px",
-                    }}
-                  >
-                    Lorem Ipsum Dolor
+                <div className="more-details">
+                  <span>Joined Recently</span>
+                  <ul>
+                    <li>
+                      <FaUserMd />
+                      <span>Dentist Specialized in Cosmetic Dentistry</span>
+                    </li>
+                    <li>
+                      <IoLocation />
+                      <span>Khaldi Street, Ibn Khaldon</span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="cta-section">
+                <div className="row">
+                  <div className="col-4">
+                    <div className="appointment-box">
+                      <h6 className="text-center">Today</h6>
+                      <div className="time-info text-center">
+                        <strong>From:</strong> 9:00 AM
+                        <br />
+                        <strong>To:</strong> 6:00 PM
+                      </div>
+                      <div className="footer">
+                        <button className="book-button">Book</button>
+                      </div>
+                    </div>
                   </div>
-                  <div
-                    className="preview-card__text"
-                    style={{
-                      color: "#4e4a67",
-                      marginBottom: "30px",
-                      lineHeight: "1.5em",
-                    }}
-                  >
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Recusandae voluptate repellendus magni illo ea animi?
+                  <div className="col-4">
+                    <div className="appointment-box">
+                      <h6 className="text-center">Tomorrow</h6>
+                      <div className="time-info text-center">
+                        <strong>From:</strong> 9:00 AM
+                        <br />
+                        <strong>To:</strong> 6:00 PM
+                      </div>
+                      <div className="footer">
+                        <button className="book-button">Book</button>
+                      </div>
+                    </div>
                   </div>
-                  <a href="#" className="preview-card__button">
-                    READ MORE
-                  </a>
+                  <div className="col-4">
+                    <div className="appointment-box">
+                      <h6 className="text-center">Date</h6>
+                      <div className="time-info text-center">
+                        <strong>From:</strong> 9:00 AM
+                        <br />
+                        <strong>To:</strong> 6:00 PM
+                      </div>
+                      <div className="footer">
+                        <button className="book-button ">Book</button>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </Link>
     </>
   );
 }
--- frontend/src/pages/contactUs.jsx
+++ frontend/src/pages/contactUs.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 function contactUs() {
-  return (
-    <div>contactUs</div>
-  )
+  return <div>contactUs</div>;
 }
 
-export default contactUs
+export default contactUs;
