f1ea3d4b58fafe901c84f37109d89cd999eb9775
AhmadNGhanimah <ahmad.n.ghanimah@gmail.com>
2024-05-14 23:57:09 +0300

    getclinicby id + get all clinic

--- frontend/src/Routers/index.jsx
+++ frontend/src/Routers/index.jsx
@@ -1,39 +1,42 @@
-import {createBrowserRouter} from  "react-router-dom"
-import Main from "../layouts/Main"
-import Home from "../pages/home"
-import Login from "../pages/login"
-import Regester from "../pages/regester"
-import ContactUs from "../pages/contactUs"
-import Clinics from "../pages/clinics"
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layouts/Main";
+import Home from "../pages/home";
+import Login from "../pages/login";
+import Regester from "../pages/regester";
+import ContactUs from "../pages/contactUs";
+import Clinics from "../pages/clinics";
+import InfoClinic from "../pages/infoClinic.jsx";
 
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "register",
+        element: <Regester />,
+      },
+      {
+        path: "contactus",
+        element: <ContactUs />,
+      },
+      {
+        path: "allClinics",
+        element: <Clinics />,
+      },
+      {
+        path: "infoClinic/:id",
+        element: <InfoClinic />,
 
-
-
-export const router =createBrowserRouter([
-    {
-        path:"/",
-        element:<Main/>,
-        children :[
-            {
-                path:"",
-                element : <Home/>
-            },
-            {
-                path:"login",
-                element:<Login/>
-            },
-            {
-                path:"register",
-                element:<Regester/>
-            },
-            {
-                path:"contactus",
-                element:<ContactUs/>
-            },
-            {
-                path:"allClinics",
-                element:<Clinics/>
-            },
-        ]
-    }
-])
+      },
+    ],
+  },
+]);
--- frontend/src/components/clinics/GetAllClinics.jsx
+++ frontend/src/components/clinics/GetAllClinics.jsx
@@ -5,8 +5,7 @@ import axios from "axios";
 import { Rating } from "react-simple-star-rating";
 import { FaUserMd } from "react-icons/fa";
 import { IoLocation } from "react-icons/io5";
-import { Link } from "react-router-dom";
-import { useParams } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import backgroundImage from "./top-clinic.png";
 import testImageDoctor from "./testImageDoctor.jpg";
 
@@ -15,12 +14,6 @@ import "./clinic.css";
 export default function ClinicSpecialization() {
   const dispatch = useDispatch();
   const clinics = useSelector((state) => state.clinic.allClinic);
-
-  const [rating, setRating] = useState(1);
-
-  const handleRating = (rating) => {
-    setRating(rating);
-  };
 
   useEffect(() => {
     axios
@@ -52,73 +45,81 @@ export default function ClinicSpecialization() {
 
       <section className="doctor container">
         {clinics.map((clinic, index) => (
-          <div className="card bg-light-subtle mt-4" key={index}>
-            <img
-              src={clinic.image_clinic}
-              className="card-img-top"
-              alt="Doctor"
-            />
-            <div className="card-body">
-              <div className="text-section">
-                <h5 className="card-title fw-bold text-capitalize">
-                  <span>Clinic</span> <a href="#">{clinic.name}</a>
-                </h5>
-                <p className="card-text">Doctor {clinic.doctor_name}</p>
-                <div className="star">
-                  <Rating initialValue={clinic.average_rating.split(":")[0]} />
+          <Link to={`/infoClinic/${clinic.id}`} key={index}>
+            <div className="card bg-light-subtle mt-4">
+              <img
+                src={clinic.image_clinic}
+                className="card-img-top"
+                alt="Doctor"
+              />
+              <div className="card-body">
+                <div className="text-section">
+                  <h5 className="card-title fw-bold text-capitalize">
+                    <span>Clinic</span> <a href="#">{clinic.name}</a>
+                  </h5>
+                  <p className="card-text">Doctor {clinic.doctor_name}</p>
+                  <div className="star">
+                    <Rating
+                      initialValue={clinic.average_rating.split(":")[0]}
+                    />
+                  </div>
+                  <div className="more-details">
+                    <span>Specialization {clinic.specialization_name}</span>
+                    <ul>
+                      <li>
+                        <FaUserMd />
+                        <span>
+                          {clinic.specialization_name +
+                            "," +
+                            clinic.description}
+                        </span>
+                      </li>
+                      <li>
+                        <IoLocation />
+                        <span>{clinic.location}</span>
+                      </li>
+                    </ul>
+                  </div>
                 </div>
-                <div className="more-details">
-                  <span>Specialization {clinic.specialization_name}</span>
-                  <ul>
-                    <li>
-                      <FaUserMd />
-                      <span>
-                        {clinic.specialization_name + "," + clinic.description}
-                      </span>
-                    </li>
-                    <li>
-                      <IoLocation />
-                      <span>{clinic.location}</span>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-              <div className="cta-section">
-                <div className="row">
-                  <div className="col-6">
-                    <div className="appointment-box">
-                      <h6 className="text-center">Today</h6>
-                      <div className="time-info text-center">
-                        <strong>From:</strong> {clinic.time_open.split(":")[0]}
-                        :00
-                        <br />
-                        <strong>To:</strong> {clinic.time_close.split(":")[0]}
-                        :00
-                      </div>
-                      <div className="footer">
-                        <button className="book-button">Book</button>
+                <div className="cta-section">
+                  <div className="row">
+                    <div className="col-6">
+                      <div className="appointment-box">
+                        <h6 className="text-center">Today</h6>
+                        <div className="time-info text-center">
+                          <strong>From:</strong>{" "}
+                          {clinic.time_open.split(":")[0]}
+                          :00
+                          <br />
+                          <strong>To:</strong> {clinic.time_close.split(":")[0]}
+                          :00
+                        </div>
+                        <div className="footer">
+                          <button className="book-button">Book</button>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                  <div className="col-6">
-                    <div className="appointment-box">
-                      <h6 className="text-center">Tomorrow</h6>
-                      <div className="time-info text-center">
-                        <strong>From:</strong> {clinic.time_open.split(":")[0]}
-                        :00
-                        <br />
-                        <strong>To:</strong> {clinic.time_close.split(":")[0]}
-                        :00
-                      </div>
-                      <div className="footer">
-                        <button className="book-button">Book</button>
+                    <div className="col-6">
+                      <div className="appointment-box">
+                        <h6 className="text-center">Tomorrow</h6>
+                        <div className="time-info text-center">
+                          <strong>From:</strong>{" "}
+                          {clinic.time_open.split(":")[0]}
+                          :00
+                          <br />
+                          <strong>To:</strong> {clinic.time_close.split(":")[0]}
+                          :00
+                        </div>
+                        <div className="footer">
+                          <button className="book-button">Book</button>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
+          </Link>
         ))}
       </section>
     </>
